1204ae03cfc9f63699240842dd241b3764b8c78b
Manfred Endres <manfred.endres@wooga.net>
2022-03-17 08:49:08 +0100

    Improve Report task herachy
    Description
    ===========
    
    When I introduced the `Report` type I made the decission to extend it
    from `Test` since it has the same interface and basic implementation.
    The issue is, if I want to filter for all `Test` tasks with
    
    ```groovy
    def allTests = project.tasks.withType(Test)
    ```
    
    then I also have all `Report` tasks in the list. I broke this link
    with this patch and reimplemented the concrete type in an abstract
    class. I also defined some more strategies to hook these tasks
    executions into a task run with the following additions:
    
    * `test_publish`
    * `report_publish`
    
    Changes
    =======
    
    * ![IMPROVE] `Report` task herachy by breaking `Test` class from `Report` class
    * ![ADD] new strategy definitions for report and test

--- src/main/groovy/wooga/gradle/snyk/tasks/Test.groovy
+++ src/main/groovy/wooga/gradle/snyk/tasks/Test.groovy
@@ -15,66 +15,10 @@
  */
 
 package wooga.gradle.snyk.tasks
-
-import org.gradle.api.tasks.Nested
-import org.gradle.internal.reflect.Instantiator
-import wooga.gradle.snyk.cli.commands.TestProjectCommandSpec
-import wooga.gradle.snyk.cli.options.CommonOption
-import wooga.gradle.snyk.cli.options.ProjectOption
-import wooga.gradle.snyk.cli.options.TestOption
-import wooga.gradle.snyk.report.SnykReports
-import wooga.gradle.snyk.report.SnykReportsImpl
-
-import javax.inject.Inject
-
 /**
  * The snyk test command checks projects for open source vulnerabilities and license issues.
  * The test command tries to auto-detect supported manifest files with dependencies and test those.
  * (https://docs.snyk.io/features/snyk-cli/commands/test)
  */
-class Test extends SnykTask implements TestProjectCommandSpec {
-
-    @Inject
-    protected Instantiator getInstantiator() {
-        throw new UnsupportedOperationException()
-    }
-
-    private SnykReports reports
-
-    @Nested
-    SnykReports getReports() {
-        reports
-    }
-
-    @Inject
-    Test() {
-        reports = instantiator.newInstance(SnykReportsImpl.class, this)
-
-        reports.sarif.outputLocation.convention(project.layout.buildDirectory.file(new File(this.temporaryDir, "report.sarif").absolutePath))
-        reports.json.outputLocation.convention(project.layout.buildDirectory.file(new File(this.temporaryDir, "report.json").absolutePath))
-
-        sarifOutputPath.convention(providers.provider({
-            if (reports.sarif.enabled) {
-                return reports.sarif.outputLocation.get()
-            }
-            null
-        }))
-
-        jsonOutputPath.convention(providers.provider({
-            if (reports.json.enabled) {
-                return reports.json.outputLocation.get()
-            }
-            null
-        }))
-    }
-
-
-    @Override
-    void addMainOptions(List<String> args) {
-        args.add("test")
-        args.addAll(getMappedOptions(this, CommonOption))
-        args.addAll(getMappedOptions(this, TestOption))
-        args.addAll(getMappedOptions(this, ProjectOption))
-        args.addAll(getMappedOptions(this, CommonOption))
-    }
+class Test extends SnykCheckBase {
 }
